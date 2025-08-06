ad1504e50284d5173f501211e347999ac4474242
asingh403 <asingh.it@hotmail.com>
2025-08-06 19:53:28 +0530

    Initial commit - AUGAPIFW2025 API Framework

--- src/test/java/com/qa/api/gorest/tests/GetUserTest.java
+++ src/test/java/com/qa/api/gorest/tests/GetUserTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 import com.qa.api.base.BaseTest;
 import com.qa.api.constants.AuthType;
+import com.qa.api.manager.ConfigManger;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -15,19 +16,20 @@ public class GetUserTest extends BaseTest{
 	
 	@Test
 	public void getAllUsersTest() {
+		System.out.println("Bearer Token from Config: " + ConfigManger.get("token"));
 		Response response = restClient.get(BASE_URL_GOREST, GOREST_USERS_ENDPOINT, null, null, AuthType.BEARER_TOKEN, ContentType.JSON);
 		Assert.assertTrue(response.statusLine().contains("OK"));
 	}
 	
 	
-	@Test
+	@Test(enabled=false)
 	public void getAllUsersWithQueryParamTest() {
 		Map<String, String> queryParams = Map.of("name", "Ashutosh", "Status", "Active"); 
 		Response response = restClient.get(BASE_URL_GOREST, GOREST_USERS_ENDPOINT, queryParams, null, AuthType.BEARER_TOKEN, ContentType.JSON);
 		Assert.assertTrue(response.statusLine().contains("OK"));
 	}
 	
-	@Test
+	@Test(enabled=false)
 	public void getSingleUsersTest() {
 		String userId="8053360";
 		Response response = restClient.get(BASE_URL_GOREST, GOREST_USERS_ENDPOINT+"/"+userId, null, null, AuthType.BEARER_TOKEN, ContentType.JSON);
